9c6fa7288075670a77a609a6cdcc83dc46c6c062
Dmitriy <dimusy@mail.ru>
2020-08-22 19:53:41 +0300

    HW_2

--- src/main/java/leeson1/AppConfig.java
+++
@@ -1,9 +0,0 @@
-package leeson1;
-
-import org.springframework.context.annotation.ComponentScan;
-import org.springframework.context.annotation.Configuration;
-
-@Configuration
-@ComponentScan
-public class AppConfig {
-}
